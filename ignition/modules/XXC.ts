536a87257dff1df0b971b81db6aa1690d22fb848
enjoy-life0000 <enjoylife.follower@gmail.com>
2024-12-11 15:08:22 -0500

    complete step  1

--- ignition/modules/XXC.ts
+++ ignition/modules/XXC.ts
@@ -1,10 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 
-const DeployMyTokenModule = buildModule("DeployXXCModule", (m) => {
+const DeployXXCModule = buildModule("DeployXXCModule", (m) => {
   const myToken = m.contract("XXC");
 
   return { myToken };
 });
 
-export default DeployMyTokenModule;
+export default DeployXXCModule;
